2e9130a77ae4cc7d4420afd0c374f72d033190df
Gasper Letnar <gasper.letnar@gmail.com>
2019-05-28 14:12:18 +0200

    uvozeni vsi potrebni podatki v tabele

--- pythondb.py
+++ pythondb.py
@@ -2,25 +2,34 @@
 # -*- encoding: utf-8 -*-
 
 import auth
-import psycopg2, psycopg2.extensions, psycopg2.extras
+import psycopg2, psycopg2.extensions, psycopg2.extras, psycopg2.sql
 import csv
 import re
-
-## vpisi ukaze za ustvarjanje/brisanje tabel, dodajanje podatkov
-
-###################
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) ## šumniki
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password) ## povezava z bazo
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) ## izvede se autocommit
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) ## na kurzorju izvedem ukaz in dobim rezultat, vedno po en pa po en
 
-###################
-
 ## python enter
 ## from pythondb import *
 ## vpisi ustvari_igra() v shell, da se ustvari tabela v bazi
 ## ce si se zmotil: conn.rollback()
+
+
+
+##############################################################
+
+## tabele s pridobljenimi podatki
+
+
+
+def pobrisi(tabela):
+    cur.execute(
+        psycopg2.sql.SQL("""
+        DROP TABLE {}
+    """).format(psycopg2.sql.Identifier(tabela)))
+    conn.commit()
 
 def ustvari_igra():
     cur.execute("""
@@ -35,11 +44,12 @@ def ustvari_igra():
             starost INT
         );
     """)
-    conn.commit()
-
-def pobrisi_igra():
     cur.execute("""
-        DROP TABLE igra;
+        ALTER TABLE igra
+        ADD COLUMN dodatek INT REFERENCES igra(serijska)
+    """)
+    cur.execute("""
+        SELECT SETVAL(pg_get_serial_sequence('igra', 'serijska'), 10000)
     """)
     conn.commit()
 
@@ -48,19 +58,214 @@ def uvozi_igra():
         rd = csv.reader(f)
         next(rd) # izpusti naslovno vrstico
         for line in rd:
-            ## ce ni podatka o starosti
+            ## ce ni podatkov
+            if line[1] == '':
+                line[1] = None
+                line[2] = None
+            elif line[2] == '':
+                line[2] = None
+            if line[3] == '':
+                line[3] = None
+                line[4] = None
+            elif line[4] == '':
+                line[4] = None
             if line[6] == '':
                 line[6] = None
-            cur.execute("""
-                INSERT INTO igra
-                (ime, min_igralcev, max_igralcev, min_cas, max_cas, leto_izdaje, starost)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                RETURNING serijska
-            """, line)
-            ## fetchone() dobi naslednji rezultat poizvedbe, prva vrednost je ID
-            lineid, = cur.fetchone()
-            print("Uvožena igra %s z ID-jem %d" % (line[0], lineid)) ## %s string %d integer ... string formating
+            # osnovna igra
+            if line[7] == '':
+                line[7] = None
+                cur.execute("""
+                    INSERT INTO igra
+                    (ime, min_igralcev, max_igralcev, min_cas, max_cas, leto_izdaje, starost, dodatek)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    RETURNING serijska
+                """, line)
+                ## fetchone() dobi naslednji rezultat poizvedbe, prva vrednost je ID
+                lineid, = cur.fetchone()
+                print("Uvožena igra %s z ID-jem %d" % (line[0], lineid)) ## %s string %d integer ... string formating
+            else:
+                cur.execute("""
+                    INSERT INTO igra
+                    (ime, min_igralcev, max_igralcev, min_cas, max_cas, leto_izdaje, starost, dodatek)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, 
+                    (SELECT serijska FROM igra WHERE ime=%s))
+                    RETURNING serijska
+                """, line)
+                ## fetchone() dobi naslednji rezultat poizvedbe, prva vrednost je ID
+                lineid, = cur.fetchone()
+                print("Uvožena igra %s z ID-jem %d" % (line[0], lineid)) ## %s string %d integer ... string formating
     conn.commit()
+
+def ustvari_zvrst():
+    cur.execute("""
+        CREATE TABLE zvrst (
+            zvrst_id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL
+        );
+    """)
+    cur.execute("""
+        SELECT SETVAL(pg_get_serial_sequence('zvrst', 'zvrst_id'), 1000)
+    """)
+    conn.commit()
+
+def uvozi_zvrst():
+    with open("tabele/zvrsti.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            cur.execute("""
+                INSERT INTO zvrst (ime)
+                VALUES (%s)
+            """, (line[0], ))
+    conn.commit()
+
+def ustvari_igrazvrst():
+    cur.execute("""
+        CREATE TABLE igrazvrst (
+            serijska INT REFERENCES igra(serijska),
+            zvrst_id INT REFERENCES zvrst(zvrst_id),
+            PRIMARY KEY(serijska, zvrst_id)
+        );
+    """)
+    conn.commit()
+
+def uvozi_igrazvrst():
+    with open("tabele/igrazvrst.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            try:
+                cur.execute("""
+                    INSERT INTO igrazvrst (serijska, zvrst_id)
+                        SELECT serijska, zvrst_id FROM igra, zvrst WHERE igra.ime=%s AND zvrst.ime=%s
+                """, line)
+            ## nekatere igre imajo vec verzij in so zato imene podvojena v excelu, tako da ce se select stavek ponovi, javimo izjemo
+            except psycopg2.errors.UniqueViolation as e:
+                print("Igra %s je že vnesena" % line[0])
+    conn.commit()
+
+def ustvari_ustvarjalci():
+    cur.execute("""
+        CREATE TABLE ustvarjalci (
+            u_id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL,
+            tip TEXT NOT NULL CHECK (tip in ('avtor', 'oblikovalec'))
+        );
+    """)
+    cur.execute("""
+        SELECT SETVAL(pg_get_serial_sequence('ustvarjalci', 'u_id'), 10000)
+    """)
+    conn.commit()
+
+def uvozi_ustvarjalci():
+    with open("tabele/avtor.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            cur.execute("""
+                INSERT INTO ustvarjalci (ime, tip)
+                VALUES (%s, 'avtor')
+            """, (line[0], ))
+    with open("tabele/oblikovalci.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            cur.execute("""
+                INSERT INTO ustvarjalci (ime, tip)
+                VALUES (%s, 'oblikovalec')
+            """, (line[0], ))
+    conn.commit()
+
+def ustvari_igraust():
+    cur.execute("""
+        CREATE TABLE igraust (
+            serijska INT REFERENCES igra(serijska),
+            u_id INT REFERENCES ustvarjalci(u_id),
+            PRIMARY KEY(serijska, u_id)
+        );
+    """)
+    conn.commit()
+
+def uvozi_igraust():
+    with open("tabele/igraavtor.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            try:
+                cur.execute("""
+                    INSERT INTO igraust (serijska, u_id)
+                        SELECT serijska, u_id FROM igra, ustvarjalci WHERE igra.ime=%s AND tip='avtor' AND ustvarjalci.ime=%s
+                """, line)
+            ## nekatere igre imajo vec verzij in so zato imene podvojena v excelu, tako da ce se select stavek ponovi, javimo izjemo
+            except psycopg2.errors.UniqueViolation as e:
+                print("Igra %s je že vnesena" % line[0])
+    with open("tabele/igraoblikovalec.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            try:
+                cur.execute("""
+                    INSERT INTO igraust (serijska, u_id)
+                        SELECT serijska, u_id FROM igra, ustvarjalci WHERE igra.ime=%s AND tip='oblikovalec' AND ustvarjalci.ime=%s
+                """, line)
+            ## nekatere igre imajo vec verzij in so zato imene podvojena v excelu, tako da ce se select stavek ponovi, javimo izjemo
+            except psycopg2.errors.UniqueViolation as e:
+                print("Igra %s je že vnesena" % line[0])
+    conn.commit()
+
+def ustvari_zalozba():
+    cur.execute("""
+        CREATE TABLE zalozba (
+            zalozba_id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL
+        );
+    """)
+    cur.execute("""
+        SELECT SETVAL(pg_get_serial_sequence('zalozba', 'zalozba_id'), 10000)
+    """)
+    conn.commit()
+
+def uvozi_zalozba():
+    with open("tabele/zalozba.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            cur.execute("""
+                INSERT INTO zalozba (ime)
+                VALUES (%s)
+            """, (line[0], ))
+    conn.commit()
+
+def ustvari_igrazal():
+    cur.execute("""
+        CREATE TABLE igrazal (
+            serijska INT REFERENCES igra(serijska),
+            zalozba_id INT REFERENCES zalozba(zalozba_id),
+            PRIMARY KEY(serijska, zalozba_id)
+        );
+    """)
+    conn.commit()
+
+def uvozi_igrazal():
+    with open("tabele/igrazalozba.csv", encoding='utf-8') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for line in rd:
+            try:
+                cur.execute("""
+                    INSERT INTO igrazal (serijska, zalozba_id)
+                        SELECT serijska, zalozba_id FROM igra, zalozba WHERE igra.ime=%s AND zalozba.ime=%s
+                """, line)
+            ## nekatere igre imajo vec verzij in so zato imene podvojena v excelu, tako da ce se select stavek ponovi, javimo izjemo
+            except psycopg2.errors.UniqueViolation as e:
+                print("Igra %s je že vnesena" % line[0])
+    conn.commit()
+
+###############################
+
+## tabele uporabnikov, sporocil in ocen
+
+
 
 def ustvari_uporabnik():
     cur.execute("""
@@ -73,49 +278,18 @@ def ustvari_uporabnik():
         )
     """)
     cur.execute("""
-        SELECT SETVAL(pg_get_serial_sequence('uporabnik', 'id'), 1000)
+        SELECT SETVAL(pg_get_serial_sequence('uporabnik', 'uporabnik_id'), 10000)
     """)
     conn.commit()
 
-def pobrisi_uporabnik():
-    cur.execute("""
-        DROP TABLE uporabnik
-    """)
-    conn.commit()
 
-def ustvari_zanr():
-    cur.execute("""
-        CREATE TABLE zanr (
-            zanr_id SERIAL PRIMARY KEY,
-            ime TEXT NOT NULL
-        );
-    """)
-    conn.commit()
 
-def pobrisi_zanr():
-    cur.execute("""
-        DROP TABLE zanr;
-    """)
-    conn.commit()
 
-def uvozi_zanr():
-    with open("tabele/zvrst.csv", encoding='utf-8') as f:
-        rd = csv.reader(f)
-        next(rd) # izpusti naslovno vrstico
-        zanri = []
-        for line in rd:
-            ## v vrstici vzamemo vse elte
-            vigri = line[1]
-            vigri = vigri.replace('[', '').replace(']', '').replace('\'', '').split(",")
-            zanri.extend(vigri)
-        zanri = set(zanri)
-        zanri.remove('')
-        for zanr in zanri:
-            cur.execute("""
-                INSERT INTO zanr (ime)
-                VALUES (%s)
-            """, [zanr])
-    conn.commit()
+###################################
+
+## dodajanje pravic
+
+
 
 def dodaj_pravice():
     cur.execute("""
